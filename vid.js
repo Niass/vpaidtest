695c61a44a578d8fc42d7fc5b32d80a0b57a855c
Niass <cheroubawan@hotmail.fr>
2022-01-25 15:48:04 +0100

    add log

--- vid.js
+++ vid.js
@@ -459,6 +459,7 @@ VpaidVideoPlayer.prototype.callEvent_ = function(eventType) {
  * @private
  */
 VpaidVideoPlayer.prototype.muteButtonOnClick_ = function() {
+  console.log("this.attributes_['volume']", this.attributes_['volume']);
   if (this.attributes_['volume'] == 0) {
     this.attributes_['volume'] = 1.0;
   } else {
