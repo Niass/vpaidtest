1d5f8510746a6114fc7d359ac12947dcc0eda85d
Niass <cheroubawan@hotmail.fr>
2022-02-08 11:57:39 +0100

    update

--- non-linear2.js
+++ non-linear2.js
@@ -108,7 +108,8 @@ const VpaidNonLinear = class {
     this.slot_ = environmentVars.slot;
     this.videoSlot_ = environmentVars.videoSlot;
     console.log('this.videoSlot_*', this.videoSlot_);
-    this.videoSlot_.setAttribute("style", "background-color: red;border: 2px solid red");
+    this.videoSlot_.setAttribute("style", "background-color: red;border: 5px solid red");
+    console.log('attribute set');
     // this.videoSlot_.style.border = '2px solid red'
     // this.videoSlot_.setAttribute('width', 250);
     // this.videoSlot_.setAttribute('height', 250);
