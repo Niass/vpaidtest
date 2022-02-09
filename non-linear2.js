71a1601d38e9035521f50be46ea300c7d1f4d27e
Niass <cheroubawan@hotmail.fr>
2022-02-09 17:27:30 +0100

    update

--- non-linear2.js
+++ non-linear2.js
@@ -116,7 +116,35 @@ const VpaidNonLinear = class {
     console.log('this.videoSlot_ type*$', this.videoSlot_.type);
 
     // this.videoSlot_.style.top = '15%';
+    if(this.videoSlot_.nodeName) {
+      var container = this.videoSlot_?.parentElement?.parentElement.parentElement.parentElement;
+      // console.log('this.videoSlot_***', this.videoSlot_);
+      // console.log('container***', container);
+//       if(container) {
+//         container.style.cssText = `
+//         transition: background 1s;
+//         background: url(https://creative.bliink.io/61fa8ba94ab26d001895b529/899fyB3.jpg) center center / cover no-repeat;
+//       inset: 0% 0% 0% 0%;
+//       z-index: 0;
+//       cursor: pointer;
+//     }
+//     `;
+  
+//     var video = container.querySelector('video');
+//     video.style.cssText = `
+//     width: auto;
+//     right: 0;
+//     top: 25%;
+//     left :auto;
+//     height: 175px;
+//     position: absolute;
 
+// `;
+//     console.log('video____', video);
+ 
+//       }
+      
+    }
 
     // Parse the incoming ad parameters.
     this.parameters_ = JSON.parse(creativeData['AdParameters']);
@@ -154,23 +182,56 @@ const VpaidNonLinear = class {
       position:absolute;
       width: 135%;
       top:5%;
-    `;
+    `
     container.style.display = 'block';
     container.style.position = 'absolute';
     container.style.width = '135%';
     container.style.bottom = '5%';
     this.slot_.appendChild(container);
-
+    
+    
     // Create an img tag and populate it with the image passed in to the ad
     // parameters.
     const adImg = document.createElement('img');
-    if (this.videoSlot_.nodeName) {
+    if(this.videoSlot_.nodeName) {
       this.slot_.appendChild(containerTwo);
       const adImgTwo = document.createElement('img');
       adImgTwo.src = overlays[1] || '';
       adImgTwo.style.margin = 'auto';
       adImgTwo.style.display = 'block';
       containerTwo.appendChild(adImgTwo);
+
+      const container = this.videoSlot_?.parentElement?.parentElement.parentElement.parentElement;
+      console.log('this.videoSlot_***', this.videoSlot_);
+      console.log('container***', container);
+      if(container) {
+        container.style.cssText = `
+        transition: background 1s;
+        background: url(https://creative.bliink.io/61fa8ba94ab26d001895b529/899fyB3.jpg) center center / cover no-repeat;
+      inset: 0% 0% 0% 0%;
+      z-index: 0;
+      cursor: pointer;
+    }
+    `;
+  
+    var video = container.querySelector('video');
+    video.style.cssText = `
+    width: auto;
+    right: 0;
+    top: 25%;
+    left :auto;
+    height: 175px;
+    position: absolute;
+
+`;
+    console.log('video____', video);
+    // video.style.width = 'auto';
+    // video.style.right = '0';
+    // video.style.top = '25%';
+    // video.style.left = 'auto';
+    // video.style.height = '175px';
+      }
+
     } else {
       console.log('type node name**', this.videoSlot_.nodeName);
       console.log('no instanceof element', this.videoSlot_);
@@ -183,33 +244,6 @@ const VpaidNonLinear = class {
     // adImg.style.marginBottom = '20px';
     adImg.addEventListener('click', this.adClick_.bind(this), false);
     container.appendChild(adImg);
-    if (this.videoSlot_.nodeName) {
-      var container = this.videoSlot_?.parentElement?.parentElement.parentElement.parentElement;
-      console.log('this.videoSlot_***', this.videoSlot_);
-      console.log('container***', container);
-      if (container) {
-        container.style.cssText = `
-        transition: background 1s;
-        background: url(https://creative.bliink.io/61fa8ba94ab26d001895b529/899fyB3.jpg) center center / cover no-repeat;
-      inset: 0% 0% 0% 0%;
-      z-index: 0;
-      cursor: pointer;
-    }
-    `;
-
-        var video = container.querySelector('video');
-        video.style.cssText = `
-    width: auto;
-    right: 0;
-    top: 25%;
-    left :auto;
-    height: 175px;
-    position: absolute;
-
-`;
-        console.log('video____', video);
-      }
-    }
 
     this.callEvent_('AdStarted');
     this.callEvent_('AdImpression');
