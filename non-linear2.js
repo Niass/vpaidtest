fbb76beee2b803dfa92cc7321bbd6f4956804bca
Niass <cheroubawan@hotmail.fr>
2022-02-09 16:29:02 +0100

    update

--- non-linear2.js
+++ non-linear2.js
@@ -196,7 +196,7 @@ const VpaidNonLinear = class {
       containerTwo.appendChild(adImgTwo);
 
     } else {
-      console.log('no instanceof element');
+      console.log('no instanceof element', this.videoSlot_);
     }
     console.log('overlays[1]', overlays[1]);
     console.log('overlays[0]', overlays[0]);
