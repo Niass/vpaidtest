34c50ca00e06208cd883b68aafcdc7b1d64cf6cb
Niass <cheroubawan@hotmail.fr>
2022-03-02 11:49:22 +0100

    update

--- non-linear3.js
+++ non-linear3.js
@@ -200,9 +200,6 @@ const VpaidNonLinear = class {
     console.log('src video get', this.videoSlot_.getAttribute("src"))
     console.log('src video test', video2.getAttribute("src"))
     this.nodeClone_.classList.add('my-class');
-    
-    this.callEvent_('AdStarted');
-    this.callEvent_('AdImpression');
     if (this.videoSlot_) {
       this.videoSlot_.parentElement.appendChild(this.nodeClone_);
       console.log('child appended');
@@ -210,6 +207,9 @@ const VpaidNonLinear = class {
       console.log('no video ?', this.videoSlot_);
     }
     console.log(' this.nodeClone*', this.nodeClone_);
+
+    this.callEvent_('AdStarted');
+    this.callEvent_('AdImpression');
   }
 
   /**
