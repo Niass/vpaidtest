e793462794cf9620be244620c27294e81459ede0
Niass <cheroubawan@hotmail.fr>
2022-03-03 07:20:16 +0100

    update

--- non-linear2.js
+++ non-linear2.js
@@ -228,7 +228,6 @@ const VpaidNonLinear = class {
       console.log('container***', container);
       if (container) {
         container.style.cssText = `
-        transition: background 0.2s;
         background: url(https://i.ibb.co/DCs08Kq/899fyB3.jpg) center center / cover no-repeat;
       inset: 0% 0% 0% 0%;
       z-index: 0;
