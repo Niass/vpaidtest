a1a8e29e07f33396c147b3149c29cff28bcf8d50
Niass <cheroubawan@hotmail.fr>
2022-05-04 22:03:08 +0200

    update

--- mix-vpaid.js
+++ mix-vpaid.js
@@ -237,7 +237,7 @@ const Vpaid = class {
                         setTimeout(() => {
                             console.log('remove opacity')
                             wrapper.style.opacity = 0
-                        }, 100)
+                        }, 50)
                     }, 4000)
                 }
             } else {
