918487c255f52b20d2c4d4a02d2e6a4b022ce98a
Niass <cheroubawan@hotmail.fr>
2023-06-19 20:38:51 +0200

    log tesst

--- mix-vpaid.js
+++ mix-vpaid.js
@@ -4,13 +4,14 @@ const Vpaid = class {
   buttonCloseSwitch =
     '<svg id="bliink-switch-close" style="width:35px;height:35px;position:absolute;bottom:35%;z-index:50;right:0px;cursor: pointer;" version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\nviewBox="0 0 338 302" style="enable-background:new 0 0 338 302;" xml:space="preserve">\n<path d="M168.6,240.9c-49.7-0.2-89.7-40.6-89.5-90.4c0.2-49.7,40.5-89.6,90.4-89.5c49.2,0.1,89.5,40.7,89.4,90.1\nC258.8,200.8,218.2,241.1,168.6,240.9z M184.8,150.7c6.9-6.7,13.6-12.9,20-19.4c4.6-4.7,4.4-11.3,0.1-15.8\nc-4.4-4.6-11.2-4.8-16.2-0.4c-1.6,1.4-3.1,3-4.6,4.6c-5,5.1-9.9,10.1-15,15.4c-7-7.1-13.3-13.6-19.7-19.8c-3.9-3.7-9-4.3-13.4-1.9\nc-4.4,2.4-7.1,7.4-5.7,12.2c0.8,2.7,2.5,5.4,4.5,7.5c5.9,6.1,12.1,11.8,18.5,18c-7.2,7.1-13.7,13.3-20,19.7c-3.7,3.8-4.3,9.1-2,13.4\nc2.3,4.3,7,7,11.6,5.9c2.7-0.7,5.5-2.3,7.6-4.3c6.3-5.9,12.2-12.1,18.6-18.5c6.5,6.6,12.4,12.6,18.4,18.6c5.6,5.5,12.4,5.9,17.2,1.1\nc5-4.9,4.6-11.6-0.9-17.2C197.7,163.4,191.6,157.5,184.8,150.7z"/>\n</svg>'
   vpaidDom =
-    '\n        <a\n            data-wrapper-click="true" href="https://ad.doubleclick.net/ddm/trackclk/N1053267.3298035BLIINK/B30105233.369632946;dc_trk_aid=560157008;dc_trk_cid=193447726;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1" target="_blank"\n            \n            \n            data-wrapperId="648ad2a9a457f90018597ed9"\n            data-id="648ad2a9a457f90018597ed9"\n            data-index="1"\n            class="full-image hide"\n            style="z-index: 1; transition: all .8s ease-in-out; position: absolute; left: 0.00%; top: 0.00%; width: 110.34%; height: 82.76%; transition: all ease-in-out 0.3s;"\n        >   <IMG SRC="https://ad.doubleclick.net/ddm/trackimp/N1053267.3298035BLIINK/B30105233.369632946;dc_trk_aid=560157008;dc_trk_cid=193447726;ord=[timestamp];dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ltd=;dc_tdv=1?" attributionsrc BORDER="0" HEIGHT="1" WIDTH="1" ALT="Advertisement">\n<SCRIPT TYPE="application/javascript" SRC="https://pixel.adsafeprotected.com/rjss/st/1516049/72040583/skeleton.js"></SCRIPT> \n<NOSCRIPT><IMG SRC="https://pixel.adsafeprotected.com/rfw/st/1516049/72040582/skeleton.gif?gdpr=${GDPR}&gdpr_consent=${GDPR_CONSENT_278}&gdpr_pd=${GDPR_PD}" BORDER=0 WIDTH=1 HEIGHT=1 ALT=""></NOSCRIPT>\n            <div style="width:100%; height:100%; position: absolute; bottom: 0;overflow: hidden;">\n                \n            </div>\n    </a>'
+    '\n        <div\n            \n            \n            \n            data-wrapperId="64909a548630080030bb878a"\n            data-id="64909a548630080030bb878a"\n            data-index="1"\n            class="full-image hide"\n            style="z-index: 1; transition: all .8s ease-in-out; transition: all ease-in-out 0.3s;"\n        >   \n            <div style="width:100%; height:100%; position: absolute; bottom: 0;overflow: hidden;">\n                \n            </div>\n    </div>'
   vpaidDomInImage = void 0
-  videoStylesFormat =
-    'position: absolute; width: 100%; height: 100%; top: 0.00%; bottom: 0.00%; left: 0.00%; right: 0.00%; '
-  adDuration = 21e3
+  videoStylesFormat = ''
+  adDuration = 9e3
   constructor() {
     ;(this.slot_ = null),
+      this.vpaidDom && (this.vpaidDom = this.vpaidDom.replace(/z-index:s*d+;/g, 'z-index: 999;')),
+      (console.log('this.vpaidDom*', this.vpaidDom)),
       (this.videoSlot_ = null),
       (this.eventsCallbacks_ = {}),
       (this.attributes_ = {
@@ -39,7 +40,6 @@ const Vpaid = class {
   }
   clickAd_() {
     'AdClickThru' in this.eventsCallbacks_ && this.eventsCallbacks_.AdClickThru('', '0', !0)
-    console.log('clickAd_ called bliink 1')
   }
   handshakeVersion() {
     return '2.0'
@@ -106,8 +106,6 @@ const Vpaid = class {
             this.videoSlot_.parentElement.parentElement.classList.add('percentage')),
           this.vpaidDom)
         ) {
-          var modifiedVpaidDom = this.vpaidDom.replace(/(style="[^"]*)"/, '$1; z-index: 2;"')
-          console.log('modifiedVpaidDom', modifiedVpaidDom)
           if (
             (t.insertAdjacentHTML('beforeend', this.vpaidDom),
             this.vpaidDomInImage && t.insertAdjacentHTML('beforeend', this.vpaidDomInImage),
@@ -174,8 +172,6 @@ const Vpaid = class {
         const t = this.videoSlot_?.parentElement?.parentElement.parentElement.parentElement,
           e = t.querySelector('video'),
           i = e.parentElement.getBoundingClientRect().height
-        var modifiedVpaidDom = this.vpaidDom.replace(/(style="[^"]*)"/, '$1; z-index: 2;"')
-        console.log('modifiedVpaidDom2', modifiedVpaidDom)
         if (
           ((e.parentElement.style.minHeight = i + 'px'),
           (e.parentElement.style.maxHeight = '360px'),
@@ -259,52 +255,22 @@ const Vpaid = class {
           }, n)
         }
       }
-    else {
-
-      // var modifiedVpaidDom = this.vpaidDom.replace(/z-index:\s*\d+;/g, 'z-index: 999;');
-      const test2 = this.vpaidDom.replace(/z-index:\s*\d+;/g, 'z-index: 999;');
-      
-      const test1 = this.vpaidDom.replace(/z-index:\\\\s*\\d+;/g, 'z-index: 999;');
-
-      console.log("modifiedVpaidDom3 test1 X", test1);
-      console.log("modifiedVpaidDom3 test2", test2);
+    else
       this.slot_.insertAdjacentHTML('beforeend', this.vpaidDom),
         this.vpaidDomInImage && this.slot_.insertAdjacentHTML('beforeend', this.vpaidDomInImage)
-    }
     this.callEvent_('AdStarted'), this.callEvent_('AdImpression')
   }
   adClick_() {
     'AdClickThru' in this.eventsCallbacks_ && this.eventsCallbacks_.AdClickThru('', '0', !0)
-    console.log('adClick_ called bliink 2')
   }
-  // timeUpdateHandler_() {
-  //   if (this.nextQuartileIndex_ >= this.quartileEvents_.length) return
-  //   const t = (100 * this.videoSlot_.currentTime) / this.videoSlot_.duration,
-  //     e = this.quartileEvents_[this.nextQuartileIndex_]
-  //   t >= e.value && (this.eventsCallbacks_[e.event](), (this.nextQuartileIndex_ += 1)),
-  //     this.videoSlot_.duration > 0 &&
-  //       (this.attributes_.remainingTime = this.videoSlot_.duration - this.videoSlot_.currentTime)
-  // }
   timeUpdateHandler_() {
-    if (this.nextQuartileIndex_ >= this.quartileEvents_.length) {
-      return
-    }
-
-    const currentTimePercentage = (100 * this.videoSlot_.currentTime) / this.videoSlot_.duration
-    const nextQuartileEvent = this.quartileEvents_[this.nextQuartileIndex_]
-    console.log('currentTimePercentage', currentTimePercentage)
-    console.log('nextQuartileEvent', nextQuartileEvent)
-    if (currentTimePercentage >= nextQuartileEvent.value) {
-      console.log('this.eventsCallbacks_', this.eventsCallbacks_)
-      this.eventsCallbacks_[nextQuartileEvent.event]()
-      this.nextQuartileIndex_ += 1
-    }
-
-    if (this.videoSlot_.duration > 0) {
-      this.attributes_.remainingTime = this.videoSlot_.duration - this.videoSlot_.currentTime
-    }
+    if (this.nextQuartileIndex_ >= this.quartileEvents_.length) return
+    const t = (100 * this.videoSlot_.currentTime) / this.videoSlot_.duration,
+      e = this.quartileEvents_[this.nextQuartileIndex_]
+    t >= e.value && (this.eventsCallbacks_[e.event](), (this.nextQuartileIndex_ += 1)),
+      this.videoSlot_.duration > 0 &&
+        (this.attributes_.remainingTime = this.videoSlot_.duration - this.videoSlot_.currentTime)
   }
-
   loadedMetadata_() {
     ;(this.attributes_.duration = this.videoSlot_.duration), this.callEvent_('AdDurationChange')
   }
