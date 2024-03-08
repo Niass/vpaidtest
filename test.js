2cd4a66c4734380560fe811dd92cbcef72a00e1d
Niass <cheroubawan@hotmail.fr>
2024-03-08 19:03:44 +0100

    test

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 window.addEventListener('message', function (event) {
   // Vérifier l'origine pour des raisons de sécurité
   console.log('Message reçu :', event.data);
+  debugger
   if (event.origin !== 'Origine autorisée') {
     return; // Ne pas traiter le message si l'origine ne correspond pas
   }
