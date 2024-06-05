2d6b4635664347bc4bd2f829e964be4b929c8ad5
Niass <cheroubawan@hotmail.fr>
2024-06-05 17:26:59 +0200

    web sdk prebid int test

---
+++ websdk.js
@@ -0,0 +1,58 @@
+!(function () {
+  var i = {
+    "mode": "ad",
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTgxODA0MDgsImlhdCI6MTcxNzU3NTYwOCwiaXNzIjoiYmxpaW5rIiwiZGF0YSI6eyJwYXJ0aXRpb25fdGltZSI6IjIwMjQtMDYtMDUgMDg6MjA6MDggVVRDIiwidHlwZSI6ImFkLXNlcnZlciIsInRyYW5zYWN0aW9uSWQiOiIwYjJiODk1ZS01MjQ4LTRhOTQtOTMzOS1mYzlkOTdiM2NjYTAiLCJuZXR3b3JrSWQiOjIxLCJzaXRlSWQiOjQ4LCJ0YWdJZCI6ODEsImV2ZW50SWQiOjMsImNyZWF0ZWRBdCI6MTcxNzU3NTYwODUxNSwidGFyZ2V0aW5nIjp7InBsYXRmb3JtIjoiV2Vic2l0ZSIsImtleXdvcmQiOiJiZXVycmUgbW91LGNob2NvbGF0IG5vaXIsY29va2llcyxmYXJpbmUgZGUgc2FycmFzaW4sbGV2dXJlIGNoaW1pcXVlLG9ldWYsc2FucyBnbHV0ZW4sc3VjcmUgYmxvbmQgZGUgY2FubmUsYmV1cnJlIG1vdSxjaG9jb2xhdCBub2lyLGNvb2tpZXMsZmFyaW5lIGRlIHNhcnJhc2luLGxldnVyZSBjaGltaXF1ZSxvZXVmLHNhbnMgZ2x1dGVuLHN1Y3JlIGJsb25kIGRlIGNhbm5lIiwicmVmZXJyZXIiOiJodHRwOi8vbG9jYWxob3N0OjgyODIvIiwicGFnZVVybCI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODI4Mi8iLCJpbWFnZVVybCI6Imh0dHBzOi8vYmxpaW5rLmlvL2ltZy9iZy9iZy0taG9tZS5qcGVnIiwidGltZSI6MTcxNzU3NTYwNywibG9jYXRpb24iOnsibGF0aXR1ZGUiOjQ4LjgzMjMsImxvbmdpdHVkZSI6Mi40MDc1LCJyZWdpb24iOiJJREYiLCJ6aXBDb2RlIjoiNzUwMTciLCJkZXBhcnRtZW50IjoiNzUifSwiY2l0eSI6IlBhcmlzIiwiY291bnRyeSI6IkZSIiwiaW50ZXJuZXRDb25uZWN0aW9uVHlwZSI6IjRnIiwiZGV2aWNlT3MiOiJtYWNPUyIsImRldmljZVBsYXRmb3JtIjoiV2Vic2l0ZSIsInJhd1VzZXJBZ2VudCI6Ik1vemlsbGEvNS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzcpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8xMDYuMC4wLjAgU2FmYXJpLzUzNy4zNiJ9LCJnZHByIjp7Imhhc0NvbnNlbnQiOmZhbHNlLCJjb25zZW50U3RyaW5nIjoibnVsbCJ9LCJ1c2VyX2lkIjoiNTU2Zjk0OTdjMDM0NWE4NmQ5NDA0NjQ2ZmNlYjA0NjZkMjI4YjYzMWQ1OWZjNjA5NDA3ODNmMjEyN2FiMmRhMCIsIm5wIjoiejl2OCt4b0lYZGxBUm5RRC81MUIwdz09IiwiZ3AiOiJmY3lqWVNNSWpaVkNPY3lOK2t3L2lnPT0iLCJhZFVuaXRQb3MiOjEsIm1hcmdpbiI6MTUsImN1cnJlbmN5IjoiRVVSIiwid2luIjpmYWxzZSwiZm9ybWF0IjoxLCJkaXNwbGF5VHlwZSI6ImFydGljbGUiLCJhZElkIjoyMTksImFkdmVydGlzZXJJZCI6MjEsImNhbXBhaWduSWQiOjUzNiwiY3JlYXRpdmVJZCI6MTU2LCJlcnJvciI6ZmFsc2V9fQ.REd6RTZy7SYVRFdhXlT_5pO9PiMc5mrIdhf6k_o2D8c",
+    "creative": {
+      "id": 156,
+      "ad_id": 219,
+      "category": 2,
+      "adm": "<iframe src=\"https://creative-stg.bliink.io/switch_test/index.html?cb=1689604769&gdpr=1&gdpr_consent=&isSticky=false&isPrebid=false#click=https://e-stg.api.bliink.io/c?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTgxODA0MDgsImlhdCI6MTcxNzU3NTYwOCwiaXNzIjoiYmxpaW5rIiwiZGF0YSI6eyJwYXJ0aXRpb25fdGltZSI6IjIwMjQtMDYtMDUgMDg6MjA6MDggVVRDIiwidHlwZSI6ImFkLXNlcnZlciIsInRyYW5zYWN0aW9uSWQiOiIwYjJiODk1ZS01MjQ4LTRhOTQtOTMzOS1mYzlkOTdiM2NjYTAiLCJuZXR3b3JrSWQiOjIxLCJzaXRlSWQiOjQ4LCJ0YWdJZCI6ODEsImV2ZW50SWQiOjMsImNyZWF0ZWRBdCI6MTcxNzU3NTYwODUxNSwidGFyZ2V0aW5nIjp7InBsYXRmb3JtIjoiV2Vic2l0ZSIsImtleXdvcmQiOiJiZXVycmUgbW91LGNob2NvbGF0IG5vaXIsY29va2llcyxmYXJpbmUgZGUgc2FycmFzaW4sbGV2dXJlIGNoaW1pcXVlLG9ldWYsc2FucyBnbHV0ZW4sc3VjcmUgYmxvbmQgZGUgY2FubmUsYmV1cnJlIG1vdSxjaG9jb2xhdCBub2lyLGNvb2tpZXMsZmFyaW5lIGRlIHNhcnJhc2luLGxldnVyZSBjaGltaXF1ZSxvZXVmLHNhbnMgZ2x1dGVuLHN1Y3JlIGJsb25kIGRlIGNhbm5lIiwicmVmZXJyZXIiOiJodHRwOi8vbG9jYWxob3N0OjgyODIvIiwicGFnZVVybCI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODI4Mi8iLCJpbWFnZVVybCI6Imh0HBzOi8vYmxpaW5rLmlvL2ltZy9iZy9iZy0taG9tZS5qcGVnIiwidGltZSI6MTcxNzU3NTYwNywibG9jYXRpb24iOnsibGF0aXR1ZGUiOjQ4LjgzMjMsImxvbmdpdHVkZSI6Mi40MDc1LCJyZWdpb24iOiJJREYiLCJ6aXBDb2RlIjoiNzUwMTciLCJkZXBhcnRtZW50IjoiNzUifSwiY2l0eSI6IlBhcmlzIiwiY291bnRyeSI6IkZSIiwiaW50ZXJuZXRDb25uZWN0aW9uVHlwZSI6IjRnIiwiZGV2aWNlT3MiOiJtYWNPUyIsImRldmljZVBsYXRmb3JtIjoiV2Vic2l0ZSIsInJhd1VzZXJBZ2VudCI6Ik1vemlsbGEvNS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzcpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8xMDYuMC4wLjAgU2FmYXJpLzUzNy4zNiJ9LCJnZHByIjp7Imhhc0NvbnNlbnQiOmZhbHNlLCJjb25zZW50U3RyaW5nIjoibnVsbCJ9LCJ1c2VyX2lkIjoiNTU2Zjk0OTdjMDM0NWE4NmQ5NDA0NjQ2ZmNlYjA0NjZkMjI4YjYzMWQ1OWZjNjA5NDA3ODNmMjEyN2FiMmRhMCIsIm5wIjoiejl2OCt4b0lYZGxBUm5RRC81MUIwdz09IiwiZ3AiOiJmY3lqWVNNSWpaVkNPY3lOK2t3L2lnPT0iLCJhZFVuaXRQb3MiOjEsIm1hcmdpbiI6MTUsImN1cnJlbmN5IjoiRVVSIiwid2luIjpmYWxzZSwiZm9ybWF0IjoxLCJkaXNwbGF5VHlwZSI6ImFydGljbGUiLCJhZElkIjoyMTksImFkdmVydGlzZXJJZCI6MjEsImNhbXBhaWduSWQiOjUzNiwiY3JlYXRpdmVJZCI6MTU2LCJlcnJvciI6ZmFsc2V9fQ.REd6RTZy7SYVRFdhXlT_5pO9PiMc5mrIdhf6k_o2D8c&redirect=\" style=\"height: 100%; width: 100%; border: 0;\"></iframe><script src=\"https://tag-stg.bliink.io/creative.min.js?cb=0.0.0\"></script>",
+      "type": 1,
+      "viewability_duration": 1,
+      "viewability_percent_in_view": 30
+    },
+    "price": 800,
+    "currency": "EUR"
+  };
+  function n() {
+    try {
+      var i;
+      if (null !== (i = window.top) && void 0 !== i && i.location.href) return !0;
+    } catch (n) {
+      return !1;
+    }
+    return !1;
+  }
+  n()
+    ? ((top.bliinkAds = top.bliinkAds || {}), (top.bliinkAds[i.extras.transaction_id] = i))
+    : ((window.bliinkAds = window.bliinkAds || {}), (window.bliinkAds[i.extras.transaction_id] = i));
+  if (n()) {
+    var t,
+      d,
+      o,
+      a = null !== (t = window.top.bliinkLoadedAds) && void 0 !== t ? t : {},
+      e = { window: window.self };
+    (e.isIframe = !!window.frameElement),
+      (a["bliink_ad_".concat(null === (d = i.extras) || void 0 === d ? void 0 : d.transaction_id)] = e),
+      (window.top.bliinkLoadedAds = a);
+    var r,
+      s,
+      c = null === (o = document.currentScript) || void 0 === o ? void 0 : o.parentElement;
+    if (!e.isIframe && c)
+      c.classList.add("bliink_ad_".concat(null === (r = i.extras) || void 0 === r ? void 0 : r.transaction_id)),
+        (c.dataset.bliinkAd = null === (s = i.extras) || void 0 === s ? void 0 : s.transaction_id);
+  }
+  var l = (function (i) {
+    var n = document.createElement("script");
+    return (
+      (n.dataset.id = i.extras.transaction_id),
+      (n.type = "text/javascript"),
+      (n.async = !0),
+      (n.src = "".concat("http://localhost:3000", "/creative.js")),
+      n
+    );
+  })(i);
+  document.addEventListener("DOMContentLoaded", function () {
+    n() ? window.top.document.body.appendChild(l) : document.body.appendChild(l);
+  });
+})();
